8e47eb7b30839f67c37a0cc7957f1a9a8289e3b7
syfllhozcan <syfllh.ozcan@gmail.com>
2023-03-30 12:58:56 +0300

    React router learn first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './Pages/About/About';
+import Projects from './Pages/Projects/Projects';
+import Contact from './Pages/Contact/Contact';
+import {Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
+
+  const Home = () => {
+    return <div className="Home"><h1>Home Sayfası</h1></div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <nav>
+        <h1 className='logo'>LOGO</h1>
+        <ul>
+          
+          <li><NavLink to="/about">About Us</NavLink></li>
+          <li><NavLink to="/projects">Prodects</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' Component={Home} />
+        <Route path='/about' Component={About} />
+        <Route path='/projects' Component={Projects} />
+        <Route path='/contact' Component={Contact} />
+      </Routes>
     </div>
+    
   );
 }
 
